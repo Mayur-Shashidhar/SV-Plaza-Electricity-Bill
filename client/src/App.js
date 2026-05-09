b91c9f48d3fc9c695ba4ea7c793786aefba44e2f
S Mayur <Mayurmamatha9916@gmail.com>
2026-05-09 15:20:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ function App() {
   const [minCharges, setMinCharges] = useState({});
 
   useEffect(() => {
-    axios.get('https://sv-plaza-electricity-bill.onrender.com').then(res => {
+    axios.get('https://sv-plaza-electricity-bill.onrender.com/api/clients').then(res => {
       setClients(res.data);
       // Initialize readings and minCharges
       const initialReadings = {};
